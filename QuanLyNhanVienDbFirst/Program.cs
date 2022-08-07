5f44edb4863f40b045e4ffde7c5615614c7f9629
Thuyen Nguyen <thuyennv@imt-soft.com>
2022-08-07 23:17:37 +0700

    commit source A 2rd

--- QuanLyNhanVienDbFirst/Program.cs
+++ QuanLyNhanVienDbFirst/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSession(cfg => { 
-    cfg.Cookie.Name = "huySession113";             
+    cfg.Cookie.Name = "huySession116666";             
     cfg.IdleTimeout = new TimeSpan(0, 60, 0);
 });
 
