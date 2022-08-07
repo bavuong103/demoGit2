e48c57a6bc2b1ac75b51a6d52e088b412b69d025
Thuyen Nguyen <thuyennv@imt-soft.com>
2022-08-07 23:42:32 +0700

    commit source A 3rd

---
+++ QuanLyNhanVienDbFirst/Controllers/GitController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace QuanLyNhanVienDbFirst.Controllers
+{
+    public class GitController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
